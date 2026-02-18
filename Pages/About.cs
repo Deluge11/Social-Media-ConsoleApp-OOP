725f00a053a674513dd76b36868af1725f2e73f9
Deluge11 <Deluge11@gmail.com>
2026-02-18 10:22:44 +0300

    Dont Click On This Commet

--- Pages/About.cs
+++ Pages/About.cs
@@ -8,15 +8,34 @@ using System.Threading.Tasks;
 
 namespace SocialApp.Pages
 {
-    public class About : AbPage
+    public class About : AbScrollPage
     {
         public override string PageName => "About";
         public override string DefaultMassage => "Default";
-        public override void SetPageContent() {
+
+        private string[] Texts { get; } =
+        {
+            "Scroll Down",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "Hire Me :)"
+          
+        };
+        public override int GetScrollContentCount()
+        {
+            return Texts.Length;
+        }
+
+        public override void SetPageContent()
+        {
             ContentGrids[1] = PageName;
-            ContentGrids[4] = "Hello";
-            ContentGrids[7] = "We are the GOATs";
-            ContentGrids[10] = "Too Much Aura";
+            ContentGrids[4] = Texts[Start];
+            ContentGrids[7] = Texts[Start + 1];
+            ContentGrids[10] = Texts[Start + 2];
         }
     }
 }
