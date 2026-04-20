5a6f318b4d6c0f5ca0838abce7cc37e6d15fcba6
Deluge11 <Deluge11@gmail.com>
2026-04-20 20:36:21 +0300

    Separate Form Components From Controller

--- Controllers/clsRendererController.cs
+++ Controllers/clsRendererController.cs
@@ -1,11 +1,8 @@
 ﻿
-using SocialApp;
 using SocialApp.Enums;
-using SocialApp.Grids;
-using SocialApp.Grids.Abstractions;
+using SocialApp.Forms;
 using SocialApp.Interfaces;
 using SocialApp.Pages.Abstractions;
-using SocialApp.Structure;
 
 
 namespace SocialApp.Controllers
@@ -14,162 +11,37 @@ namespace SocialApp.Controllers
     {
         public clsAppState AppState { get; }
         public clsNavigationController NavigationController { get; }
+        public clsMainForm Form { get; } = new clsMainForm();
 
-        //===============================================================
-
-        int[] Rows = { 0, 8, 15, 22 };
-        int[] Cols = { 0, 23, 46 };
-
-
-
-        clsTextGrid appNameContentGrid = new clsTextGrid(11, 1, new stPaddingInfo(2, 1, 2, 1));
-        clsTextGrid pageStackContentGrid = new clsTextGrid(50, 1, new stPaddingInfo(3, 1, 3, 1));
-
-        clsHorizontalLineGrid line = new clsHorizontalLineGrid(68);
-
-
-        clsGridManager pageGridManager = new clsGridManager(68, 29, new stPaddingInfo(1, 1, 1, 1));
-        clsGridManager headerBarManager = new clsGridManager(75, 3, new stPaddingInfo(0, 0, 0, 0));
-        clsGridManager scrollBarManager = new clsGridManager(6, 21, new stPaddingInfo(1, 1, 1, 1));
-        clsGridManager centerGrid = new clsGridManager(80, 31, new stPaddingInfo(1, 1, 1, 1));
-
-
-        clsTextGrid[] ContentGrids = new clsTextGrid[12];
-
-        clsVerticalScrollBarGrid pageScrollBar = new clsVerticalScrollBarGrid(2, 19, '=');
-        clsVerticalContentGrid scrollBarText = new clsVerticalContentGrid(17);
-
-        //===============================================================
 
         public clsRendererController(clsAppState appState, clsNavigationController navigationController)
         {
-            for (int i = 0; i < ContentGrids.Length; i++)
-            {
-                ContentGrids[i] = new clsTextGrid(20, 5, new stPaddingInfo(1, 1, 1, 1));
-            }
-
-            pageGridManager.AddGrid(new stGridInfo(line, new stPoint(0, 7)));
-            Print(centerGrid);
-
-            for (int row = 0, count = 0; row < Rows.Length; row++)
-            {
-                for (int col = 0; col < Cols.Length; col++, count++)
-                {
-
-                    Print(pageGridManager);
-                    pageGridManager.AddGrid(new stGridInfo(ContentGrids[count], new stPoint(Cols[col], Rows[row])));
-                }
-            }
-            Print(pageGridManager);
-
-
-            pageScrollBar.SetScrollBarInformation(0, 0, 0);
-            appNameContentGrid.Text = "Social App";
-            scrollBarText.Text = $"S C R O L L {clsCustomTags.InvisibleChar} B A R";
-
-            centerGrid.AddGrid(new stGridInfo(pageGridManager, new stPoint(0, 0)));
-            Print(centerGrid);
-
-            centerGrid.AddGrid(new stGridInfo(scrollBarManager, new stPoint(72, 8)));
-            Print(centerGrid);
-
-            scrollBarManager.AddGrid(new stGridInfo(pageScrollBar, new stPoint(0, 0)));
-            Print(centerGrid);
-
-            scrollBarManager.AddGrid(new stGridInfo(scrollBarText, new stPoint(5, 4)));
-            Print(centerGrid);
-
-            headerBarManager.AddGrid(new stGridInfo(appNameContentGrid, new stPoint(0, 0)));
-            headerBarManager.AddGrid(new stGridInfo(pageStackContentGrid, new stPoint(16, 0)));
-            Print(headerBarManager,350);
-
-            Console.Clear();
-            headerBarManager.Print();
-            centerGrid.Print();
-
-            Thread.Sleep(150);
-            Console.WriteLine("| Press ? To `???`'");
-            Thread.Sleep(150);
-            Console.WriteLine("| Press ? To `???`'");
-            Thread.Sleep(150);   
-            Console.WriteLine("| Press ? To `???`'");
-            Thread.Sleep(150);
-            Console.WriteLine("| Press ? To `???`'");
-            Thread.Sleep(150);
-            Console.WriteLine("| Press ? To `???`'");
-            Thread.Sleep(800);
-
-
-            ResetGrids();
-
-
             NavigationController = navigationController;
             AppState = appState;
         }
 
-        private void Print(absBaseGrid grid,int sleepMS = 160)
-        {
-            Console.Clear();
-            grid.Print();
-            Thread.Sleep(sleepMS);
-        }
-
-        protected void ResetGrids()
-        {
-            for (int row = 0, count = 0; row < Rows.Length; row++)
-            {
-                for (int col = 0; col < Cols.Length; col++, count++)
-                {
-                    ContentGrids[count].BorderShape = enBorderShape.None;
-                }
-            }
-        }
-
-        protected void PrintPointers()
-        {
-            if (NavigationController.GetCurrentPage() is not absScrollPage)
-            {
-                return;
-            }
-
-            Console.WriteLine("*---------------------------");
-            if (NavigationController.GetCurrentPage() is absScrollPage scrollPage)
-            {
-                Console.WriteLine($"| Start Cursor Value: {scrollPage.StartCursor}");
-            }
-            if (NavigationController.GetCurrentPage() is absScrollSelection scrollCursor)
-            {
-                Console.WriteLine($"| Selection Cursor Value: {scrollCursor.SelectionCursor}");
-            }
-            Console.WriteLine("*---------------------------");
-        }
-
         public void Print()
         {
-            ResetGrids();
-
             Console.Clear();
-            BoardProcessing();
-            headerBarManager.Print();
-            centerGrid.Print();
+            RefreshForm();
+            Form.Print();
             PrintControlKeys();
-
-            //Testing
-            //PrintPointers();
         }
 
-        protected void BoardProcessing()
+        protected void RefreshForm()
         {
-            SetPagesStackGrid();
-            SetPageContentOnBoardGrids();
-            SetCursorOnBoard();
-            SetScrollBarOnBoard();
+            DisableContentGridsBorder();
+
+            UpdatePagesStackGrid();
+            UpdateContentGrids();
+            UpdateSelectedContentGrid();
+            UpdateScrollBar();
         }
 
-        protected void SetPagesStackGrid()
+        protected void UpdatePagesStackGrid()
         {
             var pagesName = NavigationController.GetPagesNames();
-            pageStackContentGrid.Text = "Pages: " + string.Join(" -> ", pagesName);
+            Form.PageStackContentGrid.Text = "Pages: " + string.Join(" -> ", pagesName);
         }
 
         protected void PrintControlKeys()
@@ -202,48 +74,54 @@ namespace SocialApp.Controllers
             }
         }
 
-        protected void SetPageContentOnBoardGrids()
+        protected void DisableContentGridsBorder()
+        {
+            for (int i = 0; i < Form.ContentGrids.Length; i++)
+            {
+                Form.ContentGrids[i].BorderShape = enBorderShape.None;
+            }
+        }
+
+        protected void UpdateContentGrids()
         {
             absBasePage currentPage = NavigationController.GetCurrentPage();
             currentPage.ResetContent();
             string[] content = currentPage.ContentGrids;
 
-
-            for (int r = 0, count = 0; r < Rows.Length; r++)
+            for (int i = 0; i < Form.ContentGrids.Length; i++)
             {
-                for (int c = 0; c < Cols.Length; c++, count++)
-                {
-                    ContentGrids[count].Text = content[count];
-                }
+                Form.ContentGrids[i].Text = content[i];
             }
         }
 
-        protected void SetScrollBarOnBoard()
+        protected void UpdateScrollBar()
         {
-            scrollBarManager.Visible = false;
+            Form.ScrollBarManager.Visible = false;
 
             if (NavigationController.GetCurrentPage() is not absScrollPage scrollPage)
-            {
                 return;
-            }
 
-            pageScrollBar.SetScrollBarInformation(scrollPage.GetRowCount(), absScrollPage.PAGE_ROWS_LIMIT, scrollPage.StartCursor);
+            Form.PageScrollBar.SetScrollBarInformation(scrollPage.GetRowCount(), absScrollPage.PAGE_ROWS_LIMIT, scrollPage.StartCursor);
 
-            scrollBarManager.Visible = pageScrollBar.IsScrollBarNeeded();
+            Form.ScrollBarManager.Visible = Form.PageScrollBar.IsScrollBarNeeded();
         }
 
-        protected void SetCursorOnBoard()
+        protected void UpdateSelectedContentGrid()
         {
-            if (NavigationController.GetCurrentPage() is not absScrollSelection page) return;
-            if (page.GetRowCount() == 0) return;
+            if (NavigationController.GetCurrentPage() is not absScrollSelection page)
+                return;
+
+            if (page.GetRowCount() == 0)
+                return;
 
             int curserRowNumber = page.SelectionCursor - page.StartCursor + 1;
             ConvertBorderShapeToDash(curserRowNumber, 1);
 
         }
+
         protected void ConvertBorderShapeToDash(int row, int col)
         {
-            ContentGrids[(row * 3) * col].BorderShape = enBorderShape.Dash;
+            Form.ContentGrids[(row * 3) * col].BorderShape = enBorderShape.Dash;
         }
 
     }
---
+++ Forms/clsMainForm.cs
@@ -0,0 +1,102 @@
+﻿using SocialApp.Grids;
+using SocialApp.Grids.Abstractions;
+using SocialApp.Structure;
+
+
+namespace SocialApp.Forms
+{
+    public class clsMainForm
+    {
+        int[] Rows = { 0, 8, 15, 22 };
+        int[] Cols = { 0, 23, 46 };
+
+        public clsTextGrid AppNameContentGrid { get; } = new clsTextGrid(11, 1, new stPaddingInfo(2, 1, 2, 1));
+        public clsTextGrid PageStackContentGrid { get; } = new clsTextGrid(50, 1, new stPaddingInfo(3, 1, 3, 1));
+
+        clsHorizontalLineGrid Line = new clsHorizontalLineGrid(68);
+
+
+        public clsGridManager PageGridManager { get; } = new clsGridManager(68, 29, new stPaddingInfo(1, 1, 1, 1));
+        public clsGridManager HeaderBarManager { get; } = new clsGridManager(75, 3, new stPaddingInfo(0, 0, 0, 0));
+        public clsGridManager ScrollBarManager { get; } = new clsGridManager(6, 21, new stPaddingInfo(1, 1, 1, 1));
+        public clsGridManager CenterGrid { get; } = new clsGridManager(80, 31, new stPaddingInfo(1, 1, 1, 1));
+
+        public clsTextGrid[] ContentGrids { get; } = new clsTextGrid[12];
+
+        public clsVerticalScrollBarGrid PageScrollBar { get; } = new clsVerticalScrollBarGrid(2, 19, '=');
+        public clsVerticalContentGrid ScrollBarText { get; } = new clsVerticalContentGrid(17);
+
+
+        public clsMainForm()
+        {
+            for (int i = 0; i < ContentGrids.Length; i++)
+            {
+                ContentGrids[i] = new clsTextGrid(20, 5, new stPaddingInfo(1, 1, 1, 1));
+            }
+
+            PageGridManager.AddGrid(new stGridInfo(Line, new stPoint(0, 7)));
+            Print(CenterGrid);
+
+            for (int row = 0, count = 0; row < Rows.Length; row++)
+            {
+                for (int col = 0; col < Cols.Length; col++, count++)
+                {
+                    Print(PageGridManager);
+                    PageGridManager.AddGrid(new stGridInfo(ContentGrids[count], new stPoint(Cols[col], Rows[row])));
+                }
+            }
+            Print(PageGridManager);
+
+
+            PageScrollBar.SetScrollBarInformation(0, 0, 0);
+            AppNameContentGrid.Text = "Social App";
+            ScrollBarText.Text = $"S C R O L L {clsCustomTags.InvisibleChar} B A R";
+
+            CenterGrid.AddGrid(new stGridInfo(PageGridManager, new stPoint(0, 0)));
+            Print(CenterGrid);
+
+            CenterGrid.AddGrid(new stGridInfo(ScrollBarManager, new stPoint(72, 8)));
+            Print(CenterGrid);
+
+            ScrollBarManager.AddGrid(new stGridInfo(PageScrollBar, new stPoint(0, 0)));
+            Print(CenterGrid);
+
+            ScrollBarManager.AddGrid(new stGridInfo(ScrollBarText, new stPoint(5, 4)));
+            Print(CenterGrid);
+
+            HeaderBarManager.AddGrid(new stGridInfo(AppNameContentGrid, new stPoint(0, 0)));
+            HeaderBarManager.AddGrid(new stGridInfo(PageStackContentGrid, new stPoint(16, 0)));
+            Print(HeaderBarManager, 350);
+
+            Console.Clear();
+            HeaderBarManager.Print();
+            CenterGrid.Print();
+
+            Thread.Sleep(180);
+            Console.WriteLine("| Press ? To `???`");
+            Thread.Sleep(180);
+            Console.WriteLine("| Press ? To `???`");
+            Thread.Sleep(180);
+            Console.WriteLine("| Press ? To `???`");
+            Thread.Sleep(180);
+            Console.WriteLine("| Press ? To `???`");
+            Thread.Sleep(180);
+            Console.WriteLine("| Press ? To `???`");
+            Thread.Sleep(800);
+        }
+
+        public void Print()
+        {
+            HeaderBarManager.Print();
+            CenterGrid.Print();
+        }
+
+        private void Print(absBaseGrid grid, int sleepMS = 160)
+        {
+            Console.Clear();
+            grid.Print();
+            Thread.Sleep(sleepMS);
+        }
+
+    }
+}
